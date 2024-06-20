bc9b4f7481874fddd4d04b0d08682aafacab6b99
Dmitry Perchanov <dmitry.perchanov@intel.com>
2024-06-20 11:21:16 +0300

    d4xx: merge jp6 hwmc and dfu procedures
    Signed-off-by: Dmitry Perchanov <dmitry.perchanov@intel.com>

--- drivers/media/i2c/d4xx.c
+++ drivers/media/i2c/d4xx.c
@@ -433,7 +433,7 @@ struct ds5_dfu_dev {
 	enum dfu_state dfu_state_flag;
 	unsigned char *dfu_msg;
 	u16 msg_write_once;
-	unsigned char init_v4l_f;
+	// unsigned char init_v4l_f; // need refactoring
 	u32 bus_clk_rate;
 };
 
@@ -1897,6 +1897,8 @@ enum DS5_HWMC_ERR {
 	DS5_HWMC_ERR_CMD     = -1,
 	DS5_HWMC_ERR_PARAM   = -6,
 	DS5_HWMC_ERR_NODATA  = -21,
+	DS5_HWMC_ERR_UNKNOWN = -64,
+	DS5_HWMC_ERR_LAST,
 };
 
 static int ds5_get_hwmc_status(struct ds5 *state)
@@ -1916,26 +1918,11 @@ static int ds5_get_hwmc_status(struct ds5 *state)
 	if (ret || status != DS5_HWMC_STATUS_OK) {
 		if (status == DS5_HWMC_STATUS_ERR) {
 			ds5_raw_read(state, DS5_HWMC_DATA, &errorCode, sizeof(errorCode));
-			switch(errorCode) {
-			case (DS5_HWMC_ERR_CMD):
-			case (DS5_HWMC_ERR_PARAM):
-				ret = -EBADMSG;
-			break;
-			case (DS5_HWMC_ERR_NODATA):
-				ret = -ENODATA;
-			break;
-
-			default:
-				dev_dbg(&state->client->dev,
-					"%s(): HWMC failed, ret: %d, status: %x, error code: %d\n",
-					__func__, ret, status, errorCode);
-				ret = -EBADMSG;
-				break;
-			}
+			return errorCode;
 		}
 	}
 	if (!ret && (status != DS5_HWMC_STATUS_OK))
-		ret = -EBUSY;
+		ret = DS5_HWMC_ERR_LAST;
 
 	return ret;
 }
@@ -1955,7 +1942,13 @@ static int ds5_get_hwmc(struct ds5 *state, unsigned char *data,
 		dev_dbg(&state->client->dev,
 			"%s(): HWMC status not clear, ret: %d\n",
 			__func__, ret);
+		if (ret != DS5_HWMC_ERR_LAST) {
+			int *p = (int *)data;
+			*p = ret;
+			return 0;
+		} else {
 			return ret;
+		}
 	}
 
 	ret = regmap_raw_read(state->regmap, DS5_HWMC_RESP_LEN,
@@ -2672,8 +2665,6 @@ static int ds5_g_volatile_ctrl(struct v4l2_ctrl *ctrl)
 			u16 dataLen = 0;
 			u16 bufLen = ctrl->dims[0];
 			ret = ds5_get_hwmc(state, data,	bufLen, &dataLen);
-			if (ret)
-				break;
 			/* This is needed for librealsense, to align there code with UVC,
 		 	 * last word is length - 4 bytes header length */
 			dataLen -= 4;
@@ -4963,18 +4954,33 @@ static ssize_t ds5_dfu_device_read(struct file *flip,
 {
 	struct ds5 *state = flip->private_data;
 	u16 fw_ver, fw_build;
-	char msg[32];
+	char msg[64];
 	int ret = 0;
+	struct __fw_status f = {0};
 
 	if (mutex_lock_interruptible(&state->lock))
 		return -ERESTARTSYS;
-	ret |= ds5_read(state, DS5_FW_VERSION, &fw_ver);
-	ret |= ds5_read(state, DS5_FW_BUILD, &fw_build);
-	if (ret < 0)
-		goto e_dfu_read_failed;
-	snprintf(msg, sizeof(msg) ,"DFU info: \tver:  %d.%d.%d.%d\n",
+	if (state->dfu_dev.dfu_state_flag == DS5_DFU_RECOVERY) {
+		/* Read device info in recovery mode */
+		ret = ds5_dfu_detach(state);
+		if (ret < 0)
+			goto e_dfu_read_failed;
+		ret = ds5_dfu_get_dev_info(state, &f);
+		if (ret < 0)
+			goto e_dfu_read_failed;
+		snprintf(msg, sizeof(msg) ,
+			 "DFU info: \trecovery:  %02x%02x%02x%02x%02x%02x\n",
+			 f.ivcamSerialNum[0], f.ivcamSerialNum[1], f.ivcamSerialNum[2],
+			 f.ivcamSerialNum[3], f.ivcamSerialNum[4], f.ivcamSerialNum[5] );
+	} else {
+		ret |= ds5_read(state, DS5_FW_VERSION, &fw_ver);
+		ret |= ds5_read(state, DS5_FW_BUILD, &fw_build);
+		if (ret < 0)
+			goto e_dfu_read_failed;
+		snprintf(msg, sizeof(msg) ,"DFU info: \tver:  %d.%d.%d.%d\n",
 			(fw_ver >> 8) & 0xff, fw_ver & 0xff,
 			(fw_build >> 8) & 0xff, fw_build & 0xff);
+	}
 
 	if (copy_to_user(buffer, msg, strlen(msg)))
 		ret = -EFAULT;
@@ -5016,7 +5022,8 @@ static ssize_t ds5_dfu_device_write(struct file *flip,
 			goto dfu_write_error;
 		}
 		state->dfu_dev.dfu_state_flag = DS5_DFU_IN_PROGRESS;
-		state->dfu_dev.init_v4l_f = 1;
+	/* find a better way to reinitialize driver from recovery to operational */
+		// state->dfu_dev.init_v4l_f = 1;
 	/* fallthrough - procceed to download */
 	__attribute__((__fallthrough__));
 	case DS5_DFU_IN_PROGRESS: {
@@ -5054,7 +5061,8 @@ static ssize_t ds5_dfu_device_write(struct file *flip,
 				goto dfu_write_error;
 			state->dfu_dev.dfu_state_flag = DS5_DFU_DONE;
 		}
-		dev_notice(&state->client->dev, "%s(): DFU block (%d) bytes written\n",
+		if (len)
+			dev_notice(&state->client->dev, "%s(): DFU block (%d) bytes written\n",
 				__func__, (int)len);
 		break;
 	}
@@ -5183,10 +5191,12 @@ static int ds5_dfu_device_release(struct inode *inode, struct file *file)
 	state->dfu_dev.device_open_count--;
 	if (state->dfu_dev.dfu_state_flag != DS5_DFU_RECOVERY)
 		state->dfu_dev.dfu_state_flag = DS5_DFU_IDLE;
-	if (state->dfu_dev.dfu_state_flag == DS5_DFU_DONE
-			&& state->dfu_dev.init_v4l_f)
-		ds5_v4l_init(state->client, state);
-	state->dfu_dev.init_v4l_f = 0;
+	/* We disable this section as it has no effect when device in operational
+	   mode and has not enough effect when device in recovery mode */
+	// if (state->dfu_dev.dfu_state_flag == DS5_DFU_DONE
+	// 		&& state->dfu_dev.init_v4l_f)
+	// 	ds5_v4l_init(state->client, state);
+	// state->dfu_dev.init_v4l_f = 0;
 	if (state->dfu_dev.dfu_msg)
 		devm_kfree(&state->client->dev, state->dfu_dev.dfu_msg);
 	state->dfu_dev.dfu_msg = NULL;
@@ -5748,6 +5758,8 @@ static int ds5_probe(struct i2c_client *c)
 	if (rec_state == 0x201) {
 		dev_info(&c->dev, "%s(): D4XX recovery state\n", __func__);
 		state->dfu_dev.dfu_state_flag = DS5_DFU_RECOVERY;
+		/* Override I2C drvdata with state for use in remove function */
+		i2c_set_clientdata(c, state);
 		return 0;
 	}
 
@@ -5762,8 +5774,6 @@ static int ds5_probe(struct i2c_client *c)
 	ret = ds5_v4l_init(c, state);
 	if (ret < 0)
 		goto e_chardev;
-	/* Override I2C drvdata */
-	/* i2c_set_clientdata(c, state); */
 
 /*	regulators? clocks?
  *	devm_regulator_bulk_get(&c->dev, DS5_N_SUPPLIES, state->supplies);
@@ -5804,10 +5814,15 @@ static int ds5_remove(struct i2c_client *c)
 static void ds5_remove(struct i2c_client *c)
 #endif
 {
-	struct ds5 *state = container_of(i2c_get_clientdata(c), struct ds5, mux.sd.subdev);
-
 #ifdef CONFIG_VIDEO_D4XX_SERDES
 	int i, ret;
+#endif
+	struct ds5 *state = container_of(i2c_get_clientdata(c), struct ds5, mux.sd.subdev);
+	if (state && !state->mux.sd.subdev.v4l2_dev) {
+		state = i2c_get_clientdata(c);
+	}
+
+#ifdef CONFIG_VIDEO_D4XX_SERDES
 	for (i = 0; i < MAX_DEV_NUM; i++) {
 		if (serdes_inited[i] && serdes_inited[i] == state) {
 			serdes_inited[i] = NULL;
@@ -5852,15 +5867,18 @@ static void ds5_remove(struct i2c_client *c)
 		regulator_disable(state->vcc);
 //	gpio_free(state->pwdn_gpio);
 
-	if (state->dfu_dev.dfu_state_flag != DS5_DFU_RECOVERY) {
+	if (state->dfu_dev.dfu_state_flag != DS5_DFU_RECOVERY && \
+		 state->mux.sd.subdev.v4l2_dev) {
 #ifdef CONFIG_SYSFS
 		sysfs_remove_group(&c->dev.kobj, &ds5_attr_group);
 #endif
 		ds5_mux_remove(state);
-		if (state->is_depth) {
-			ds5_chrdev_remove(state);
-		}
 	}
+
+	if (state->is_depth && state->dfu_dev.ds5_class) {
+		ds5_chrdev_remove(state);
+	}
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
 	return 0;
 #endif
@@ -5909,4 +5927,4 @@ MODULE_AUTHOR("Guennadi Liakhovetski <guennadi.liakhovetski@intel.com>,\n\
 				Shikun Ding <shikun.ding@intel.com>");
 MODULE_AUTHOR("Dmitry Perchanov <dmitry.perchanov@intel.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("1.0.2.20");
+MODULE_VERSION("1.0.1.23");
